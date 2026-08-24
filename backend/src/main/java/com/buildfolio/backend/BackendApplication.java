d0b765cc2171cccdfbd655780c5563a6feb17ce7
kumarmanish562 <qmanishkumar562@gmail.com>
2026-08-24 14:36:57 +0530

    feat: restructure backend and client projects, add database configuration, and implement theme and query providers

--- backend/src/main/java/com/buildfolio/backend/BackendApplication.java
+++ backend/src/main/java/com/buildfolio/backend/BackendApplication.java
@@ -1,4 +1,4 @@
-package devPilot.backend;
+package com.buildfolio.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
